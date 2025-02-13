50b881556d5bbc250aeaf4e29dde742c56a2a834
gamerjuniorjr07@gmail.com <gamerjuniorjr07@gmail.com>
2025-02-13 08:06:11 -0300

    V2

---
+++ desafio/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+
+import Header from './Components/Header';
+function Header() {
+
+    return(
+        <>
+        
+        <img src="/src" alt="LogoG1" />
+        </>
+    )
+}
+
+export default Header;
