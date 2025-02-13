dd84de71d3be97b3d8613816e953c044261c6910
Felipe Cagnin <cagnin.lima.2007@gmail.com>
2025-02-13 08:56:45 -0300

    V3

--- desafio/src/App.jsx
+++ desafio/src/App.jsx
@@ -1,24 +1,23 @@
-import Footer from './components/footer'
-import MeioDP from './components/Meio'
-import Img from './assets/gmail.png'
-import './App.css'
+import Footer from "./components/footer";
+import MeioDP from "./components/Meio";
+import Img from "./assets/gmail.png";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-        <section className="meiasso">
-<MeioDP nome = "GrupOne001"></MeioDP>
-</section>
+      <section className="meiasso">
+        <MeioDP nome="GrupOne001"></MeioDP>
+      </section>
 
-    <Footer text = "GrupOne001@gmail.com"
-    img = {Img}
-    texto = "Copyright 2021 GrupOne001"
-    imge = {Img}
-    /> 
-
+      <Footer
+        text="GrupOne001@gmail.com"
+        img={Img}
+        texto="Copyright 2021 GrupOne001"
+        imge={Img}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
