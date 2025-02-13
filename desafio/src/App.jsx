bb3e20ce19ec98b5341cc5123fa4a1d871396b16
Murilo Correia <mucorreia008@gmail.com>
2025-02-13 09:55:10 -0300

    meio

--- desafio/src/App.jsx
+++ desafio/src/App.jsx
@@ -2,13 +2,24 @@ import Footer from './components/footer'
 import MeioDP from './components/Meio'
 import Img from './assets/gmail.png'
 import './App.css'
+import Felipe from './assets/felipe.jpg'
+import MuriloC from './assets/murilocorreia.jpeg'
+import MuriloB from './assets/murilobesson.jpg'
+import VitorH from './assets/vitorhugo.jpg'
+import William from './assets/william.jpg'
 
 function App() {
 
   return (
     <>
         <section className="meiasso">
-<MeioDP nome = "GrupOne001"></MeioDP>
+<MeioDP nome = "Felipe Cagnin" image = {Felipe} hobby = "Flauta"></MeioDP>
+<MeioDP nome = "Murilo Correia" image = {MuriloC} hobby = "Música"></MeioDP>
+<MeioDP nome = "Vitor Hugo" image = {VitorH} hobby = "Futebol"></MeioDP>
+<MeioDP nome = "Murilo Besson" image = {MuriloB} hobby = "Jogar"></MeioDP>
+<MeioDP nome = "William Magno" image = {William} hobby = "Namorada"></MeioDP>
+
+
 </section>
 
     <Footer text = "GrupOne001@gmail.com"
